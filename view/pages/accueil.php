8e3f924bd0a74abea6349b14f539766ec261619b
Sebastien Bargier <sebastien.bargier@laplateforme.io>
2022-01-07 11:53:33 +0100

    page accueil

---
+++ view/pages/accueil.php
@@ -0,0 +1,25 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../../public/css/style.css" />
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <?php require '../common/header.php'; ?>
+    </header>
+
+    <main>
+        <h1>Page Accueil<h1>
+    </main>
+
+    <footer>
+        <?php require '../common/footer.php'; ?>
+    </footer>
+</body>
+</html>
