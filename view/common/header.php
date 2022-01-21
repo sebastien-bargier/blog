73a7b4d2f370bc34ffa423dc98af66d6ef9eeea7
Sebastien Bargier <sebastien.bargier@laplateforme.io>
2022-01-21 18:16:55 +0100

    mise à jour header, footer, css

--- view/common/header.php
+++ view/common/header.php
@@ -1,22 +1,28 @@
 <nav>
-  <ul>
-    <li><a href="accueil.php">Accueil</a></li>
+  
+  <a class="logo" href="accueil.php">eBLOG</a>
 
-    <?php if($_SESSION) : ?>
-        <li><a href="profil.php">Profil</a></li>
-        <li><a href="deconnexion.php">Déconnexion</a></li>
+  <ul>
+    
+    <?php if(isset($_SESSION['id'])) : ?>
+      
+      <li><a href="profil.php">Profil</a></li>
+      <li><a href="../common/deconnexion.php">Déconnexion</a></li>
 
     <?php else : ?>
-        <li><a href="inscription.php">Inscription</a></li>
-        <li><a href="connexion.php">Connexion</a></li>
+        
+      <li><a href="inscription.php">Inscription</a></li>
+      <li><a href="connexion.php">Connexion</a></li>
 
     <?php endif ?>
 
     <li class="deroulant"><a href="articles.php">Articles</a>
       <ul class="sous">
+        
         <li><a href="">Categorie 1</a></li>
         <li><a href="">Categorie 2</a></li>
         <li><a href="">Categorie 3</a></li>
+
       </ul>
     </li>
   </ul>
