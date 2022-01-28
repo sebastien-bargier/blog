33045bdf9a8c0f1fe43dcbfd590fa989fbf0c170
Sebastien Bargier <sebastien.bargier@laplateforme.io>
2022-01-28 14:49:01 +0100

    formulaire choix categorie fonctionnel, MAJ pagination

--- view/pages/articles.php
+++ view/pages/articles.php
@@ -8,105 +8,112 @@ session_start();
 
 require '../common/config.php';
 
-// DETERMINE SUR QUELLE PAGE ON SE TROUVE
-if(isset($_GET['page']) && !empty($_GET['page'])) {
+// On détermine sur quelle page on se trouve
+if(isset($_GET['start']) && !empty($_GET['start'])) {
 
-    $pageCourante = (int) strip_tags($_GET['page']);
+    $currentPage = (int) strip_tags($_GET['start']);
+
 }else{
 
-    $pageCourante = 1;
+    $currentPage = 1;
+}
+// On détermine dans quelle catégorie on se trouve
+if (isset($_GET['categorie']) && !empty($_GET['categorie'])) {
+
+    $categorie_chiffre = 'WHERE id_categorie = '.strip_tags($_GET['categorie']);
+}
+else {
+    $categorie_chiffre = '';
 }
 
-// COMPTE LE NOMBRE ARTICLE TOTAL EN BDD
-$req = $db->prepare("SELECT COUNT(*) AS compteur FROM articles");
-$req->execute();
 
-// RECUPERE LE NOMBRE D'ARTICLE
-$result = $req->fetch();
-$nombreArticlesBDD = (int) $result['compteur'];
+// On détermine le nombre total d'articles
+$requete = $db->prepare('SELECT  COUNT(*) AS nb_articles FROM `articles` '.$categorie_chiffre.'');
 
-// NOMBRE TOTAL ARTICLES PAR PAGE
-$articlesParPage = 3;
-
-// CALCUL NOMBRE DE PAGE TOTAL
-$nombreDePages = ceil($nombreArticlesBDD/$articlesParPage);
-
-// SPECIFIER LA VALEUR DE DEPART
-$premierArticle = ($pageCourante * $articlesParPage) - $articlesParPage;
-
-// REQUETE RECUPERATION ARTICLES PAR ORDRE DU PLUS RECENTS AU PLUS ANCIENS (DESC)
-$requete = $db->prepare("SELECT * FROM categories INNER JOIN articles ON articles.id_categorie = categories.id ORDER BY date DESC LIMIT $premierArticle, $articlesParPage;");
+// On exécute
 $requete->execute();
-$articles = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+// On récupère le nombre d'articles
+$result = $requete->fetch();
+
+$nbArticles = (int) $result['nb_articles'];
+
+// On détermine le nombre d'articles par page
+$parPage = 5;
+
+// On calcule le nombre de pages total
+$pages = ceil($nbArticles / $parPage);
+
+// Calcul du 1er article de la page
+$premier = ($currentPage * $parPage) - $parPage;
+
+$requete = $db->prepare('SELECT * FROM `articles` '.$categorie_chiffre.' ORDER BY `id` DESC LIMIT '.$premier.', '.$parPage.'');
+$requete->execute();
+$articles = $requete->fetchAll();
 
 // REQUETE CHOIX CATEGORIE 
 $filtreCat = $db->prepare("SELECT * FROM categories");
 $filtreCat->execute(array());
 
-if(isset($_POST['formsend'])) {
-
-    $_SESSION['formsend'] = $_POST['formsend'];
-    $_SESSION['categorie'] = (int)$_POST['categorie'];
-}
-
-$requete = $db->prepare("SELECT * FROM categories INNER JOIN articles ON articles.id_categorie = categories.id WHERE articles.id_categorie = :selectCategorie ORDER BY date DESC LIMIT $premierArticle, $articlesParPage;");
-$requete->execute(array('selectCategorie' => $_SESSION['categorie']));
-$articles = $requete->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
-<!doctype html>
-<html lang="fr">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Les articles</title>
-<link rel="stylesheet" href="../../public/css/style.css">
-<link rel="icon" href="favicon.ico" />
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../public/css/style.css">
+    <title>Document</title>
 </head>
 <body>
 
-    <!--Import du header -->
+<header>
 
-    <header>
+<?php include ('../common/header.php'); ?>
 
-    <?php include ('../common/header.php'); ?>
- 
-    </header>
+</header>
 
     <main>
 
-        <form class="categorie_form" action="" method="POST">
-
-            <select name="categorie">
-
-                <option value="0">Toutes les catégories</option>
-
-                <?php
-                
-                while($valCat = $filtreCat->fetch(PDO::FETCH_ASSOC)) {
-                                                
-                    echo "<option value=" . $valCat["id"] . ">" . $valCat["nom"] . "</option>";
-                                                
-                }
-
-                ?>
-
-                <input type="submit" class="categorie_btn" name="formsend" value="OK">
-
-            </select>
-
-        </form>
-
         <section>
- 
+
+            <!-- Continents -->
+            <section class="categories">
+
+            <form class="categorie_form" action="" method="GET">
+
+                <select name="categorie">
+
+                    <option value="0">Toutes les catégories</option>
+
+                    <?php
+                    
+                    while($valCat = $filtreCat->fetch(PDO::FETCH_ASSOC)) {
+                                                    
+                        echo "<option value=" . $valCat["id"] . ">" . $valCat["nom"] . "</option>";
+                                                    
+                    }
+
+                    ?>
+
+                    <input type="submit" class="categorie_btn" name="formsend" value="OK">
+
+                </select>
+
+            </form>
+
+            </section>
+
+            <!-- Articles -->
+            <section class="les_articles">
+    
             <?php foreach($articles as $article) : ?>
 
                 <article>
 
                     <a href="../pages/article.php?id=<?= $article['id'] ?>">
-
-                        <p>Categorie - <?= $article['nom'] ?></p>
 
                         <h3><?= $article['titre'] ?></h3>
 
@@ -120,56 +127,58 @@ $articles = $requete->fetchAll(PDO::FETCH_ASSOC);
                 </article>
             <?php endforeach; ?>
 
+            </section>
+
+            <!-- Pagination -->
+            <section class="pagination">
+
+                <?php
+
+                if ($currentPage != 1) {
+
+                    echo '<a href="articles.php?';
+                    
+                    if (isset($_GET['categorie'])) {
+                        echo 'categorie='.$_GET['categorie'].'&';
+                    } 
+
+                    echo 'start='.($currentPage-1).'">Précédant</a>'; 
+                } 
+                
+                for($page = 1; $page <= $pages; $page++) {
+                    
+                    
+                    if($page == $currentPage) {
+
+                        echo $page.' ';
+                    } else {
+
+                        echo '<a href="articles.php?';
+                        
+                        if (isset($_GET['categorie'])) {
+                            echo 'categorie='.$_GET['categorie'].'&';
+                        }  
+
+                        echo 'start='.$page.'">'.$page.'</a> ';
+                    }
+                }
+
+                if ($currentPage < $pages) {
+
+                    echo '<a href="articles.php?';
+                    
+                    if (isset($_GET['categorie'])) {
+
+                        echo 'categorie='.$_GET['categorie'].'&';
+                    }   
+                    
+                    echo 'start='.($currentPage+1).'">Suivant</a>';
+                }
+
+                ?>
+            </section>
         </section>
-
-        <div class="pagination">
-
-            <?php
-
-            if($_SESSION['formsend']) {
-
-                if($pageCourante != 1) {
-
-                    $precedent = $pageCourante-1;
-                    echo'<a href="articles.php?categorie='. $_SESSION['categorie'] .'&page='.$precedent.'">precedent</a>';   
-                }
-
-                for($page = 1; $page <= $nombreDePages; $page++) {
-
-                    echo'<a href="articles.php?categorie='. $_SESSION['categorie'] .'&page='.$page.'">'.$page.'</a>';   
-                }
-
-                if($pageCourante<$nombreDePages) {
-
-                    $suivant= $pageCourante+1;
-                    echo'<a href="articles.php?categorie='. $_SESSION['categorie'] .'&page='.$suivant.'">suivant</a>';   
-                }
-            
-            } else {
-  
-                if($pageCourante != 1) {
-
-                    $precedent = $pageCourante-1;
-                    echo'<a href="articles.php?page='.$precedent.'">precedent</a>'; 
-
-                }
-
-                for($page=1; $page <= $nombreDePages; $page++) {
-            
-                    echo'<a href="articles.php?page='.$page.'">'.$page.'</a>';       
-                }
-
-                if($pageCourante<$nombreDePages) {
-
-                    $suivant= $pageCourante+1;
-                    echo'<a href="articles.php?page='.$suivant.'">suivant</a>';   
-                }
-            }
-
-            ?>
-            
-        </div>
-
     </main>
+
 </body>
 </html>
