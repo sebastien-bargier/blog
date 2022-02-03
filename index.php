160e1a3e5b547c81959de10424fa17f919579d2a
Sebastien Bargier <sebastien.bargier@laplateforme.io>
2022-02-03 10:13:46 +0100

    MAJ index

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 <?php
 
 // Redirection vers la page d'accueil
 
 header('location: ./view/pages/accueil.php');
 
-=======
-<?php
-
-// Redirection vers la page d'accueil
-
-header('location: ./view/pages/accueil.php');
-
->>>>>>> sebastien-bargier
 ?>
